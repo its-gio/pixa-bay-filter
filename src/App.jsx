ce1585611ed5946b04583b4497af8e9f12426841
its-gio <me@its-g.io>
2019-02-04 14:45:26 -0800

    Setting up new project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>This is working</header>
+      </div>
+    );
+  }
+}
+
+export default App;
